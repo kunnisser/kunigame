7a9376fbfef6f313f4cd530a09548af3d2b10a98
kunnisser <kunnisser@sina.cn>
2019-07-22 16:11:16 +0800

    添加目录

--- kuni/main.js
+++ kuni/main.js
@@ -1,3 +1,12 @@
 import * as PIXI from 'pixi.js';
 
 window.PX = PIXI;
+
+const view = document.getElementById('view');
+
+const app = new PIXI.Application({
+	width: 1200,
+	height: 800,
+});
+
+view.appendChild(app.view);
