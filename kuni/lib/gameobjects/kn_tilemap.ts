65ba637511d0c8a1b65feae9ac9d95a82c950851
kunnisser <kunnisser@sina.cn>
2019-09-17 17:45:17 +0800

    设置地图镜头

--- kuni/lib/gameobjects/kn_tilemap.ts
+++ kuni/lib/gameobjects/kn_tilemap.ts
@@ -6,15 +6,13 @@ class TileMap extends PIXI['tilemap'].CompositeRectTileLayer{
   public alias: Object;
   public textures: Array<PIXI.Texture>;
   public size: number;
-  public tileWidth: number;
   public mapData: Array<number>;
   position: any;
-  constructor (zindex: Number, textures: Array<PIXI.Texture>, alias: Object, size: number, tileWidth: number) {
+  constructor (zindex: Number, textures: Array<PIXI.Texture>, alias: Object, size: number) {
     super(0, textures);
     this.alias = alias;
     this.textures = textures;
     this.size = size;
-    this.tileWidth = tileWidth;
     this.mapData = [];
     this.buildTilemap();
     return this;
@@ -28,7 +26,7 @@ class TileMap extends PIXI['tilemap'].CompositeRectTileLayer{
 
     // 遍历data数据，根据layer数据中的类型下标添加对应的texture。
     for (let md of this.mapData) {
-      this.addFrame(textures[md - 1], this.size * (index % this.tileWidth), this.size * ~~(index / this.tileWidth));
+      this.addFrame(textures[md - 1], this.size * (index % this.size), this.size * ~~(index / this.size));
       index += 1;
     }
   }
