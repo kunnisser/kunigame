3d44a013b47295284d8ba5bff98275ae807653ba
kunnisser <kunnisser@sina.cn>
2021-02-16 15:43:03 +0800

    操作台缩放控制1.0 完成

--- projects/kuni/lib/dev/editor_mask/cover.ts
+++ projects/kuni/lib/dev/editor_mask/cover.ts
@@ -2,8 +2,8 @@
  * @Author: kunnisser
  * @Date: 2021-02-04 16:00:55
  * @LastEditors: kunnisser
- * @LastEditTime: 2021-02-12 12:43:15
- * @FilePath: \kunigame\projects\kuni\lib\dev\editor_mask\cover.ts
+ * @LastEditTime: 2021-02-16 15:42:30
+ * @FilePath: /kunigame/projects/kuni/lib/dev/editor_mask/cover.ts
  * @Description: ---- 编辑蒙层 ----
  */
 
@@ -17,7 +17,10 @@ class CoverMask extends KnGroup {
   public curTip_Y: number;
   public start_X: number;
   public start_Y: number;
+  public move_X: number;
+  public move_Y: number;
   public isDragging: boolean;
+  public actionMask: Graphics;
   constructor(game: Game, parent: PIXI.Container) {
     super(game, 'coverMask', parent);
     this.game = game;
@@ -25,13 +28,16 @@ class CoverMask extends KnGroup {
     this.start_Y = 0;
     this.curTip_X = 0;
     this.curTip_Y = 0;
+    this.move_X = 0;
+    this.move_Y = 0;
     this.isDragging = false;
     this.initial();
   }
 
   initial() {
     this.generateGrid();
-    this.bindControllerHandler();
+    this.actionMask = this.addActionArea();
+    this.bindControllerHandler(this.actionMask);
   }
 
   /**
@@ -83,11 +89,25 @@ class CoverMask extends KnGroup {
   }
 
   /**
+   * @description: 添加操作蒙版
+   * @param {*}
+   * @return {Graphics} actionMask 返回操作蒙层 
+   */
+  addActionArea(): Graphics {
+    let actionMask: Graphics = this.game.add.graphics();
+    actionMask.hitArea = new PIXI.Rectangle(0, 0, this.game.config.width, this.game.config.height);
+    actionMask.interactive = true;
+    this.addChild(actionMask);
+    actionMask.position.set(0, 0);
+    return actionMask;
+  }
+
+  /**
    * @description: 绑定操作监听事件集
    * @param {*}
    * @return {*}
    */
-  bindControllerHandler(): void {
+  bindControllerHandler(mask: Graphics): void {
     const SCALE_VALUE: number = 1000;
     let scaleVal: number = SCALE_VALUE;
     // 原先画布的缩放
@@ -95,7 +115,9 @@ class CoverMask extends KnGroup {
 
     // 缩放系数
     let scale: number = 1;
+
     const canvas: any = this.game.view.children[0];
+
     // 绑定move事件
     const posTextTip = this.game.add.text(``, {
       fontSize: 14,
@@ -103,73 +125,68 @@ class CoverMask extends KnGroup {
       fill: 0x8ac007,
       stroke: 0xffffff,
       strokeThickness: 6,
-    }, [0, 0.5]);
+    }, [0.5, 0.5]);
 
+    // 控制鼠标划入面板监听事件
+    let mouseIn: boolean = false;
 
-    // 定义当前视图的坐标
-    let _posX: number = 0;
-    let _posY: number = 0;
-    // canvas.addEventListener('mousedown', (e: MouseEvent) => {
-    //   this.isDragging = false;
-    //   this.start_X = e.offsetX;
-    //   this.start_Y = e.offsetY;
-    //   moveX = this.game.world.x;
-    //   moveY = this.game.world.y;
-    // });
-    // document.documentElement.addEventListener('mouseup', (e) => {
-    //   this.isDragging = false;
-    //   moveX = this.game.world.x;
-    //   moveY = this.game.world.y;
-    // });
-
-    canvas.addEventListener('mouseout', (e: MouseEvent) => {
-      posTextTip.visible = false;
+    mask.on('mouseout', (e: MouseEvent) => {
+      mouseIn = false;
+      posTextTip.visible = mouseIn;
     });
 
-    canvas.addEventListener('mousemove', (e: MouseEvent) => {
+    mask.on('mouseover', (e: any) => {
+      mouseIn = true;
+      posTextTip.visible = mouseIn;
+    });
+
+    mask.on('mousemove', (e) => {
+      if (!mouseIn) {
+        return;
+      }
       posTextTip.visible = true;
-      this.curTip_X = e.offsetX / (COVER_SCALE * scale);
-      this.curTip_Y = e.offsetY / (COVER_SCALE * scale);
-      const tip_X = this.curTip_X;
-      const tip_Y = this.y / (COVER_SCALE * scale) + this.curTip_Y;
-      console.log(this.width / COVER_SCALE - this.game.config.width);
-      console.log('-----');
+      this.curTip_X = e.data.global.x / (COVER_SCALE * scale);
+      this.curTip_Y = e.data.global.y / (COVER_SCALE * scale);
 
+      const textTip_X: number = ~~(this.move_X + this.curTip_X);
+      const textTip_Y: number = ~~(this.move_Y + this.curTip_Y);
 
-
-      posTextTip.text = `${~~tip_X}, ${~~tip_Y}`;
-      posTextTip.position.set(tip_X, tip_Y);
+      posTextTip.text = `${textTip_X}, ${textTip_Y}`;
+      posTextTip.position.set(textTip_X, textTip_Y);
     });
-    // 绑定wheel事件
+
+    // 绑定缩放事件
     canvas.addEventListener('wheel', (e: WheelEvent) => {
+      posTextTip.visible = false;
       scaleVal -= e.deltaY;
       if (scaleVal < SCALE_VALUE) {
         scaleVal = SCALE_VALUE;
       }
       scale = scaleVal / SCALE_VALUE;
 
-
-      this.pivot.set(this.curTip_X, this.curTip_Y);
-      this.position.set(this.curTip_X * COVER_SCALE, this.curTip_Y * COVER_SCALE);
-      this.game.world.pivot.set(this.curTip_X, this.curTip_Y);
-      this.game.world.position.set(this.curTip_X * COVER_SCALE, this.curTip_Y * COVER_SCALE);
-
+      // 缩放画布及控制台
       this.scale.set(COVER_SCALE * scale);
       this.game.world.scale.set(COVER_SCALE * scale);
-      const tip_X = this.x * COVER_SCALE + this.curTip_X;
-      const tip_Y = this.y * COVER_SCALE + this.curTip_Y;
-      posTextTip.text = `${~~tip_X}, ${~~tip_Y}`;
-      posTextTip.position.set(tip_X, tip_Y);
 
+      // 改变缩放锚点
+      this.pivot.set(posTextTip.x, posTextTip.y);
+      this.position.set(posTextTip.x * COVER_SCALE, posTextTip.y * COVER_SCALE);
+      this.game.world.pivot.set(posTextTip.x, posTextTip.y);
+      this.game.world.position.set(posTextTip.x * COVER_SCALE, posTextTip.y * COVER_SCALE);
+
+      // 计算缩放产生的相对位置偏离
+      this.move_X = this.toLocal(this.game.stage.position).x;
+      this.move_Y = this.toLocal(this.game.stage.position).y;
+
+      // 缩小为初始尺寸重置定位
       if (scale == 1) {
-        _posX = 0;
-        _posY = 0;
         this.game.world.pivot.set(0, 0);
         this.game.world.position.set(0, 0);
         this.pivot.set(0, 0);
         this.position.set(0, 0);
       }
     });
+
     this.addChild(posTextTip);
   }
 }
