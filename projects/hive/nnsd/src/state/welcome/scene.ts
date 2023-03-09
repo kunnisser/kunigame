655dc4ed278e91a60bd93c0415505aa6dcfbe862
kunnisser <kunnisser@sina.cn>
2023-03-09 14:00:31 +0800

    drag工具移动事件绑定代码编辑

--- projects/hive/nnsd/src/state/welcome/scene.ts
+++ projects/hive/nnsd/src/state/welcome/scene.ts
@@ -35,42 +35,46 @@ class Welcome extends KnScene {
     bg.width = this.game.config.width;
     bg.height = this.game.config.height;
     const logo: Sprite = this.game.add.image("logo", this);
-    logo.y = 453;
-    logo.x = 353;
+    logo.y = 292;
+    logo.x = 1454;
     logo.anchor.set(0.5, 0.5);
     const demoText: KnText = this.game.add.text("demoText", "WELCOME", {}, [0.5, 0.5]);
+    demoText.scale.y = 0.5;
+    demoText.scale.x = 0.5;
     demoText.visible = true;
     demoText.style.dropShadowDistance = 16;
     demoText.style.dropShadowBlur = 10;
-    demoText.style.dropShadowAngle = 1.7;
+    demoText.style.dropShadowAngle = 0.6;
     demoText.style.dropShadow = true;
     demoText.style.dropShadowColor = "#57d8d8";
     demoText.style.fill = "#ffffff";
     demoText.style.strokeThickness = 20;
-    demoText.style.stroke = "#df0000";
+    demoText.style.stroke = "#2a5860";
     demoText.text = "cams";
-    demoText.x = 1129;
-    demoText.y = this.game.config.half_h;
+    demoText.x = 912;
+    demoText.y = 244;
     demoText.style.fontSize = 500;
     const demo1Text: KnText = this.game.add.text("demo1Text", "测试", {
       fontSize: "24",
       fill: 0xffffff
     }, [0.5, 0.5]);
-    demo1Text.x = 1129;
-    demo1Text.y = 696;
-    demo1Text.scale.y = 2.8;
-    demo1Text.scale.x = 4.2;
+    demo1Text.text = "by.kunnisser";
+    demo1Text.x = 1292;
+    demo1Text.y = 724;
+    demo1Text.scale.y = 1;
+    demo1Text.scale.x = 1;
     const bmText = this.game.add.bitmapText("bmText", "153112312312313123\n23012313212313\n1", {
       fontSize: 35,
       fontName: "Desyrel"
     });
+    bmText.align = "center";
     bmText.anchor.set(0.5, 0.5);
-    bmText.y = 344;
-    bmText.x = 1137;
+    bmText.y = 553;
+    bmText.x = 870;
     bmText.letterSpacing = 11;
     bmText.fontSize = 100;
     bmText.fontName = "Desyrel";
-    bmText.text = "cams";
+    bmText.text = "somewhere over\nthe rainbow";
     this.addChild(demoText);
     this.addChild(demo1Text);
     this.addChild(bmText);
--- server/route/project/path/index.js
+++ server/route/project/path/index.js
@@ -2,36 +2,32 @@
  * @Author: kunnisser
  * @Date: 2021-03-07 21:38:50
  * @LastEditors: kunnisser
- * @LastEditTime: 2023-03-07 00:15:13
- * @FilePath: \kunigame\server\route\project\path\index.js
+ * @LastEditTime: 2023-03-09 09:24:32
+ * @FilePath: /kunigame/server/route/project/path/index.js
  * @Description: ---- 项目路径汇总 ----
  */
-const path = require('path');
-const os = require('os');
+const path = require("path");
 
 // 游戏项目目录路径
-const basePath =
-  os.type() == 'Darwin'
-    ? path.normalize(process.cwd())
-    : path.normalize(path.resolve(process.cwd(), '..'));
+const basePath = path.normalize(process.cwd());
 
 // 项目工程路径
-const projectBasePath = path.normalize(path.resolve(basePath, 'projects'));
+const projectBasePath = path.normalize(path.resolve(basePath, "projects"));
 
 // 项目模板路径
 const sourcePath = `${projectBasePath}/template`;
 
 // 项目仓库路径
-const hivePath = path.normalize(path.resolve(projectBasePath, 'hive'));
+const hivePath = path.normalize(path.resolve(projectBasePath, "hive"));
 
 // 所展示的编辑器画布路径
 const gameWorkBenchPath = path.normalize(
-  path.resolve(basePath, 'editor/page/workbench/canvas.tsx')
+  path.resolve(basePath, "editor/page/workbench/canvas.tsx")
 );
 
 module.exports = {
   projectBasePath,
   sourcePath,
   hivePath,
-  gameWorkBenchPath,
+  gameWorkBenchPath
 };
